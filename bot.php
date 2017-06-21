8e8b60a9db4df2fbbcbebc741c100cb260d33277
thawatchai-sena <dream_9286@hotmail.com>
2017-06-21 16:24:07 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -22,6 +22,9 @@ if (!is_null($events['events'])) {
 				'text' => $text
 			];
 
+//https://arty16db.herokuapp.com/Insert.php?msg=""
+			
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -41,6 +44,32 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 
 			echo $result . "\r\n";
+			
+// ตัวแปรข้อมูล SERVER MySQL สำหรับเชื่อมต่อ
+$servername = "180.180.43.255";
+$username = "root";
+$password = "T4cmQLSesETWynRP";
+$dbname = "arty16_news";
+// Create connection สร้างการเชื่อมต่อ
+$conn = new mysqli($servername, $username, $password, $dbname);
+    
+// ทำให้บันทึกข้อมูลลง MySQL เป็นภาษาไทยได้
+$conn->set_charset("utf8");
+    
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "INSERT INTO tblNews (date, subject, details,section)
+VALUES ('2017-06-21', $text, 'เมื่อ....', 'ฝกบ.')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+
+
 		}
 	}
 }
