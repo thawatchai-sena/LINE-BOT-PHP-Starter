0d01d51142dd1e23071faf60c428d147bddc909e
thawatchai-sena <dream_9286@hotmail.com>
2017-06-21 16:28:10 +0700

    Update Line-api.php

--- Line-api.php
+++ Line-api.php
@@ -43,6 +43,29 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
  
+// ตัวแปรข้อมูล SERVER MySQL สำหรับเชื่อมต่อ
+$servername = "180.180.43.255";
+$username = "root";
+$password = "T4cmQLSesETWynRP";
+$dbname = "arty16_news";
+// Create connection สร้างการเชื่อมต่อ
+$conn = new mysqli($servername, $username, $password, $dbname);
+    
+// ทำให้บันทึกข้อมูลลง MySQL เป็นภาษาไทยได้
+$conn->set_charset("utf8");
+    
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+$sql = "INSERT INTO tblNews (date, subject, details,section)
+VALUES ('2017-06-21', $arrJson['events'][0]['message']['text'], 'เมื่อ....', 'ฝกบ.')";
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
  
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
