660c270ac669952034033840d23ba56c082f940d
thawatchai-sena <dream_9286@hotmail.com>
2019-01-09 06:50:49 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -11,5 +11,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-
-echo $result;
+echo "OK";
+//echo $result;
